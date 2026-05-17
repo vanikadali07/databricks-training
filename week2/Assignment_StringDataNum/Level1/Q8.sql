25198b3c0a8f79f6e35752202aa1c863dd5d155c
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:22:16 +0530

    Implement name length vs salary correlation analysis
    Add SQL query to analyze correlation between name length and salary.

--- week2/Assignment_StringDataNum/Level1/Q8.sql
+++ week2/Assignment_StringDataNum/Level1/Q8.sql
@@ -1,1 +1,21 @@
+--Q8: Display name length vs salary correlation analysis with name length,
+--    years of service,
+--    rounded salary,
+--    comparison between name length and years,
+--    and bias classification.
 
+SELECT 
+       emp_id,
+       emp_name,
+       LENGTH(emp_name) AS name_length,
+       TIMESTAMPDIFF(YEAR, join_date, CURDATE()) AS years_of_service,
+       ROUND(salary) AS rounded_salary,
+       LENGTH(emp_name) -
+       TIMESTAMPDIFF(YEAR, join_date, CURDATE()) AS length_year_difference,
+       CASE
+            WHEN LENGTH(emp_name) >
+                 TIMESTAMPDIFF(YEAR, join_date, CURDATE())
+                 THEN 'Name Bias'
+            ELSE 'Neutral'
+       END AS bias_status
+FROM name_salary;
