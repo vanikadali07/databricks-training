bedff3a15a56a9e2a300fa0a04dc897131b5e863
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:55:12 +0530

    Update Q7.sql

--- week2/Assignment_StringDataNum/Q7.sql
+++ week2/Assignment_StringDataNum/Q7.sql
@@ -1,1 +1,28 @@
+--Q: Display product discount validation with proper case product name,
+--   discount amount,
+--   discount percentage,
+--   sale day name,
+--   and discount status classification.
 
+SELECT 
+       product_id,
+
+       INITCAP(product_name) AS product_name,
+
+       ABS(mrp - selling_price) AS discount_amount,
+
+       ROUND(((mrp - selling_price) / mrp) * 100, 2) AS discount_percentage,
+
+       DAYNAME(sale_date) AS sale_day,
+
+       CASE
+            WHEN selling_price < mrp
+                 THEN 'Valid Discount'
+
+            WHEN selling_price > mrp
+                 THEN 'Overpriced'
+
+            ELSE 'No Discount'
+       END AS discount_status
+
+FROM product_sales;
