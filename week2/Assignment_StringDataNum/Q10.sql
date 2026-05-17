9fe5d88e87ead4dd2e08431f47f45a9292e2de9d
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:52:49 +0530

    Update Q10.sql

--- week2/Assignment_StringDataNum/Q10.sql
+++ week2/Assignment_StringDataNum/Q10.sql
@@ -1,1 +1,21 @@
+--Q: Display customer account status evaluation with absolute balance,
+--   days since last transaction,
+--   proper case branch name,
+--   sign of balance,
+--   and account status classification.
 
+SELECT 
+       account_id,
+       customer_name,
+       ABS(balance) AS absolute_balance,
+       DATEDIFF(CURDATE(), last_transaction) AS days_since_transaction,
+       INITCAP(branch) AS branch_name,
+       SIGN(balance) AS balance_sign,
+       CASE
+            WHEN balance < 0
+                 THEN 'Overdrawn'
+            WHEN DATEDIFF(CURDATE(), last_transaction) > 180
+                 THEN 'Dormant'
+            ELSE 'Active'
+       END AS account_status
+FROM bank_accounts;
