19fc83c5d894999785501f0f7eb15b74b266311c
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:19:56 +0530

    Update Q5.sql

--- week2/Assignment_StringDataNum/Level1/Q5.sql
+++ week2/Assignment_StringDataNum/Level1/Q5.sql
@@ -1,1 +1,19 @@
+--Q: Display odd-even salary compliance analysis with lowercase employee name,
+--    weekday extraction,
+--    rounded salary,
+--    MOD calculation on salary,
+--    and compliance status.
 
+SELECT 
+       emp_id,
+       LOWER(emp_name) AS employee_name,
+       DAYNAME(payment_date) AS weekday_name,
+       ROUND(salary) AS rounded_salary,
+       MOD(ROUND(salary), 2) AS salary_mod_value,
+       CASE
+            WHEN MOD(ROUND(salary), 2) = 0
+                 AND MOD(DAY(payment_date), 2) = 1
+                 THEN 'Violation'
+            ELSE 'Compliant'
+       END AS compliance_status
+FROM payroll_control;
