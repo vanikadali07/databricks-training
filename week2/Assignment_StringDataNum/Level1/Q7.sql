8528f83099286de6763503fc1872f0d9dec50528
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:21:00 +0530

    Add salary integrity check query with employee details
    Query retrieves employee details with salary integrity checks.

--- week2/Assignment_StringDataNum/Level1/Q7.sql
+++ week2/Assignment_StringDataNum/Level1/Q7.sql
@@ -1,1 +1,20 @@
+--Q7: Display salary sign integrity check with uppercase employee name,
+--    extracted year,
+--    SIGN value of salary,
+--    absolute salary,
+--    and salary validation status.
 
+SELECT 
+       emp_id,
+       UPPER(emp_name) AS employee_name,
+       YEAR(record_date) AS record_year,
+       SIGN(salary) AS salary_sign,
+       ABS(salary) AS absolute_salary,
+       CASE
+            WHEN SIGN(salary) = -1
+                 THEN 'Negative Error'
+            WHEN SIGN(salary) = 0
+                 THEN 'Zero Salary'
+            ELSE 'Valid'
+       END AS salary_status
+FROM salary_integrity;
