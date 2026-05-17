f2cf9c3b00b79316c3304fc97378dabb4dcdbb5e
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:14:27 +0530

    Implement salary symmetry check SQL query
    Add SQL query for salary symmetry check with proper name formatting and symmetry classification.

--- week2/Assignment_StringDataNum/Level1/Q15.sql
+++ week2/Assignment_StringDataNum/Level1/Q15.sql
@@ -1,1 +1,19 @@
+--Q15: Display salary symmetry check with integer salary extraction,
+--     reversed salary digits, weekday extraction, proper case name,
+--     and symmetry classification.
 
+SELECT 
+       emp_id,
+       CONCAT(
+            UPPER(LEFT(emp_name, 1)),
+            LOWER(SUBSTRING(emp_name, 2))
+       ) AS proper_emp_name,
+       FLOOR(salary) AS integer_salary,
+       REVERSE(CAST(FLOOR(salary) AS CHAR)) AS reversed_salary,
+       DAYNAME(processed_date) AS processed_day,
+       CASE
+            WHEN CAST(FLOOR(salary) AS CHAR) = REVERSE(CAST(FLOOR(salary) AS CHAR))
+                 THEN 'Symmetric Pay'
+            ELSE 'Asymmetric'
+       END AS symmetry_status
+FROM salary_symmetry;
