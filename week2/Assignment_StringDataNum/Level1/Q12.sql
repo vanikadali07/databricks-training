1a22652e4c2932c98ba2a56dab8ef2bd0a1f024c
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:11:55 +0530

    Update Q12.sql

--- week2/Assignment_StringDataNum/Level1/Q12.sql
+++ week2/Assignment_StringDataNum/Level1/Q12.sql
@@ -1,1 +1,18 @@
+--Q12: Display salary credit time drift analysis with hour extraction,
+--     lowercase employee name, floored salary, salary-hour difference,
+--     and credit time classification.
 
+SELECT 
+       emp_id,
+       LOWER(emp_name) AS lower_emp_name,
+       HOUR(credit_ts) AS credit_hour,
+       FLOOR(salary) AS floored_salary,
+       (FLOOR(salary) - HOUR(credit_ts)) AS salary_hour_difference,
+       CASE
+            WHEN HOUR(credit_ts) BETWEEN 0 AND 3
+                 THEN 'Midnight Drift'
+            WHEN HOUR(credit_ts) > 3 AND HOUR(credit_ts) < 9
+                 THEN 'After Hours'
+            ELSE 'Business Hours'
+       END AS time_classification
+FROM salary_time_drift;
