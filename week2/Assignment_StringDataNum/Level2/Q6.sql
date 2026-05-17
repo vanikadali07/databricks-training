9118e8f3391c533f0d8902c2c91cb1682dbbdad7
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:29:36 +0530

    Update Q6.sql

--- week2/Assignment_StringDataNum/Level2/Q6.sql
+++ week2/Assignment_StringDataNum/Level2/Q6.sql
@@ -1,1 +1,28 @@
+--Q: Display employee attendance evaluation with lowercase employee name,
+--   attendance percentage,
+--   month name,
+--   absent days calculation,
+--   and attendance performance classification.
 
+SELECT 
+       emp_id,
+
+       LOWER(emp_name) AS employee_name,
+
+       MONTHNAME(record_date) AS month_name,
+
+       ROUND((present_days / total_days) * 100, 2) AS attendance_percentage,
+
+       (total_days - present_days) AS absent_days,
+
+       CASE
+            WHEN ((present_days / total_days) * 100) >= 90
+                 THEN 'Excellent'
+
+            WHEN ((present_days / total_days) * 100) BETWEEN 75 AND 89
+                 THEN 'Average'
+
+            ELSE 'Poor'
+       END AS attendance_status
+
+FROM attendance;
