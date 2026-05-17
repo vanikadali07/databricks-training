076a9c3b71fb87949d93a738391dff7382d4c3e4
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:19:22 +0530

    Implement salary analysis query in Q4.sql
    Add SQL query for salary digit pattern analysis.

--- week2/Assignment_StringDataNum/Level1/Q4.sql
+++ week2/Assignment_StringDataNum/Level1/Q4.sql
@@ -1,1 +1,18 @@
+--Q: Display salary digit pattern analysis with last two characters of employee name,
+--   day of month from credit date,
+--   truncated salary,
+--   MOD calculation on salary,
+--   and pattern match classification.
 
+SELECT 
+       emp_id,
+       RIGHT(emp_name, 2) AS last_two_characters,
+       DAY(credit_date) AS credit_day,
+       TRUNCATE(salary, 0) AS truncated_salary,
+       MOD(TRUNCATE(salary, 0), 10) AS salary_mod_value,
+       CASE
+            WHEN MOD(TRUNCATE(salary, 0), 10) = DAY(credit_date)
+                 THEN 'Pattern Match'
+            ELSE 'No Match'
+       END AS pattern_status
+FROM salary_digits;
