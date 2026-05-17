a36270722f82902f896fe59a3ebed1dd17206a8a
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:22:43 +0530

    Update Q9.sql

--- week2/Assignment_StringDataNum/Level1/Q9.sql
+++ week2/Assignment_StringDataNum/Level1/Q9.sql
@@ -1,1 +1,17 @@
+--Q9: Display salary spike detection by month with month name,
+--    CEIL salary,
+--    last day of month validation,
+--    and salary spike classification.
 
+SELECT 
+       emp_id,
+       emp_name,
+       MONTHNAME(paid_date) AS month_name,
+       CEIL(salary) AS ceil_salary,
+       LAST_DAY(paid_date) AS last_day_of_month,
+       CASE
+            WHEN paid_date = LAST_DAY(paid_date)
+                 THEN 'End Month Spike'
+            ELSE 'Regular'
+       END AS spike_status
+FROM salary_monthly;
