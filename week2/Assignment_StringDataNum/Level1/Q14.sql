310192643fa1cc47fb9b55c426f17072b1e2e12f
vanikadali07 <kadalivani27@gmail.com>
2026-05-17 22:13:34 +0530

    Add salary growth calculation and classification query
    Query calculates projected salary growth and classifies growth status.

--- week2/Assignment_StringDataNum/Level1/Q14.sql
+++ week2/Assignment_StringDataNum/Level1/Q14.sql
@@ -1,1 +1,29 @@
+--Q14: Display salary growth power index with years since last hike,
+--     exponential growth calculation using POWER(),
+--     projected salary rounding, uppercase employee name,
+--     and growth classification.
 
+SELECT 
+       emp_id,
+       UPPER(emp_name) AS upper_emp_name,
+       base_salary,
+       growth_rate,
+       FLOOR(DATEDIFF(CURDATE(), last_hike) / 365) AS years_since_hike,
+       ROUND(
+            base_salary * POWER(growth_rate, FLOOR(DATEDIFF(CURDATE(), last_hike) / 365)),
+            0
+       ) AS projected_salary,
+
+       CASE
+            WHEN (
+                 base_salary * POWER(growth_rate, FLOOR(DATEDIFF(CURDATE(), last_hike) / 365))
+                 ) > 150000
+                 THEN 'Explosive Growth'
+            WHEN (
+                 base_salary * POWER(growth_rate, FLOOR(DATEDIFF(CURDATE(), last_hike) / 365))
+                 ) BETWEEN 100000 AND 150000
+                 THEN 'Controlled'
+            ELSE 'Stagnant'
+       END AS growth_status
+
+FROM salary_growth;
