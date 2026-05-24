1ef8505904a4db87cbe7ce6b6b1d13b165b59857
vanikadali07 <kadalivani27@gmail.com>
2026-05-24 11:32:52 +0530

    Update Session1.py

--- week3/Session1.py
+++ week3/Session1.py
@@ -1,1 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ##Reading and displaying a file
 
+# COMMAND ----------
+
+dff=spark.read.format("csv").option("header","true").option("inferSchema","true").load("/Volumes/workspace/default/databricks/Big Sales.csv")
+dff.display()
+
+# COMMAND ----------
+
+df=spark.read.format("csv").option("header","true").option("inferSchema","true").load("/Volumes/workspace/default/databricks/empData.csv")
+df.display()
